81f62b7f4612498b30891b491a94f0dbabe86ee1
Matthew Self <dmz@mself.com>
2017-05-01 15:51:30 -0700

    Updated to use new JSON configuration format

--- box-node-webhook-to-lambda-sample/index.js
+++ box-node-webhook-to-lambda-sample/index.js
@@ -1,32 +1,24 @@
 /**
- * This sample shows how to connect a Box webhook to an AWS Lambda function via API Gateway.
+ * This sample shows how to connect a Box webhook to a Lambda function via API Gateway.
  *
- * Each time an event occurs that triggers the webhook on Box, the Lambda function will be called with the details
- * of the event. The messages are secured with a message signature that is validated in the Lambda function.
- *
- * The sample Box test event is signed with the keys 'SamplePrimaryKey' and 'SampleSecondaryKey'.
- * To process events from your Box application, replace these keys with your keys (from the Box developer console),
- * configure the API Gateway to have 'Open' security and create a Box webhook that specifies your API Gateway URL
- * (from the Triggers tab).
- *
- * For step-by-step instructions, see box-node-webhook-to-lambda-sample in https://github.com/box/samples
+ * For step-by-step instructions on how to create a Box application and a webhook,
+ * see https://github.com/box/samples/tree/master/box-node-webhook-to-lambda-sample
  */
 
 'use strict';
 
 const BoxSDK = require('box-node-sdk');
 
-// Load the application secrets from environment variables for security and configuration management
-const primarySignatureKey = process.env.BOX_WEBHOOK_PRIMARY_SIGNATURE_KEY;
-const secondarySignatureKey = process.env.BOX_WEBHOOK_SECONDARY_SIGNATURE_KEY;
+// Load the config from an environment variable for security and configuration management.
+const boxConfig = JSON.parse(process.env.BOX_CONFIG);
 
 /**
  *  YOUR CODE GOES HERE!!!
  *
  *  This sample function simply logs details of the webhook event to Cloudwatch.
  *  Your code could forward the event to SNS or Kinesis for further processing.
- *  For FILE.UPLOADED events, you could use the Box Node SDK to download the file, analyze it, and update the
- *  file on Box with metadata that contains the results of the analysis.
+ *  For FILE.UPLOADED events, you could use the Box Node SDK to download the file, analyze it,
+ *  and update the file on Box with metadata that contains the results of the analysis.
  */
 function handleWebhookEvent(webhookEvent) {
     // Print basic information about the Box event
@@ -47,13 +39,14 @@ function handleWebhookEvent(webhookEvent) {
 }
 
 /**
- * The event handler validates the message using the signing keys to ensure that the message was sent from
- * your Box application.
+ * The event handler validates the message using the signing keys to ensure that the message
+ * was sent from your Box application before calling handleWebhookEvent().
  */
 exports.handler = (event, context, callback) => {
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
 
-    if (!BoxSDK.validateWebhookMessage(event.body, event.headers, primarySignatureKey, secondarySignatureKey)) {
+    if (!BoxSDK.validateWebhookMessage(event.body, event.headers,
+            boxConfig.webhooks.primaryKey, boxConfig.webhooks.secondaryKey)) {
         const response = { statusCode: 403, body: 'Message authenticity not verified' };
         console.log(`Response: ${JSON.stringify(response, null, 2)}`);
         callback(null, response);
